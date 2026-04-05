d1123d961ee08a434079fa7b866869ecdfaeb96f
Axel Dovskog <ax0825do-s@student.lu.se>
2026-04-05 19:14:03 +0200

    Start on API

---
+++ python-backend/api/api/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+"""
+timestamp: datetime
+status: int
+message: str
+level: str
+service: str
+"""
+
+# Create your models here.
+class LogEvent(models.Model):
+    timestamp = models.DateTimeField()
+    status = models.PositiveIntegerField()
+    message = models.CharField(max_length=512)
+    level = models.CharField(max_length=64)
+    service = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"[{self.level}] {self.service} ({self.message})"
